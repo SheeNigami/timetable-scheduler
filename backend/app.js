40ddad95a5c54bf2409ac9cb0a9f12c02b30cf01
shini <sheenhern@outlook.com>
2020-05-18 19:10:25 +0800

    app.js exports

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+// app.use(express.static('../frontend', {index: 'index.html'}))
 
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 app.use(bodyParser.json());
 app.use(urlencodedParser);
 
-
+module.exports = app;
