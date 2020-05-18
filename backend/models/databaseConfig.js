796ee38f535f4cf3e5c84a489c35cadb5b3a3a72
shini <sheenhern@outlook.com>
2020-05-19 01:29:17 +0800

    Switched to postgres-promise

--- backend/models/databaseConfig.js
+++ backend/models/databaseConfig.js
@@ -1,7 +1,12 @@
-var pg = require('pg');
+// var pg = require('pg');
+var pgp = require('pg-promise')({
+    capSQL: true 
+});
 
 var conString = "postgres://vjlwiciy:CIkK-1B9JBbAet-PFyQLoKttC9B0Je75@arjuna.db.elephantsql.com:5432/vjlwiciy" //Can be found in the Details page
-var client = new pg.Client(conString);
+// var client = new pg.Client(conString);
+const client = pgp(conString);
+
 client.connect(function(err) {
   if(err) {
     return console.error('could not connect to postgres', err);
