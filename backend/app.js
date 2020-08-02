899906bef04d72b2ed7267669ce23391acc0fda3
shini <sheenhern@outlook.com>
2020-08-03 04:18:29 +0800

    added advanced bulk insert, no data check, some comments

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+// Libraries
 var express = require('express');
 var app = express();
 app.use(express.static('../frontend', {index: 'index.html'}));
@@ -7,8 +8,11 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 app.use(bodyParser.json());
 app.use(urlencodedParser);
 
+// Models
 var Lectures = require('./models/lectures.js');
+var Technicians = require('./models/technicians.js')
 
+// Get all basic data
 app.get('/basic/data', (req, res) => {
     Lectures.getAllLectures().then((allLectures) => {
         res.status(200).send(allLectures);
@@ -18,9 +22,11 @@ app.get('/basic/data', (req, res) => {
     });
 });
 
+// Bulk insert for basic
 app.post('/basic/insert', (req, res) => {
     const cols = ['lectureId', 'facultyId', 'semesterId', 'dayOfWeek', 'startTime', 'endTime'];
     let data = req.body.data;
+
     if (!(checkInputs(cols, data))) {
         res.status(500).send({"error": "Invalid input(s)", "code": 1});
     } else {
@@ -33,13 +39,35 @@ app.post('/basic/insert', (req, res) => {
     }
 }); 
 
+// Bulk insert for advanced
+app.post('/advance/insert', (req, res) => {
+    const cols = ['technicianId', 'semesterId', 'facultyId', 'dayOfWeek', 'startTime', 'endTime'];
+    let data = req.body.data;
+    
+    if (!(checkInputs(cols, data))) {
+        res.status(500).send({"error": "Invalid input(s)", "code": 1});
+    } else {
+        Lectures.insertLectures(data).then(() => {
+            res.status(201).send({'results': 'success'});
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send({"error": err.detail, 'code': err.code});
+        });
+    }
+})
+
+// Input validation for basic insert
 function checkInputs(cols, data) {
+    // Check is no data/empty data
+    if (typeof(data) == 'undefined' || data == [] || data == {}) {
+        return false;
+    }
     for (const row of data) {
         for (const col of cols) {
             if (!(col in row)) {
                 return false;
             }
-            if (col == 'lectureId' || col == 'facultyId' || col == 'semesterId') {
+            if (col == 'lectureId' || col == 'technicianId' || col == 'facultyId' || col == 'semesterId') {
                 if(!(checkTenDigitId(row[col]))) {
                     console.log(row[col]);
                     return false;
