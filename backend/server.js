f195da51c4e42b7a1c369920b29af22d8d387d7f
shini <sheenhern@outlook.com>
2020-08-04 17:09:51 +0800

    changed log port string

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,5 @@ var app = require('./app.js');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("App hosted at localhost: " + 3000);
+    console.log("App hosted at localhost: " + PORT);
 });
